4174bcd207a768a26bd5657e2cec93cfe41d0a13
maisonn <37782167+maisonn@users.noreply.github.com>
2018-03-27 14:20:12 +0300

    update

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,4 +1,8 @@
 // This is where it all goes :)
-function NextPage() {
-	location.replace("forwho.html")
+function openNav() {
+    document.getElementById("myBottomnav").style.height = "15%";
+}
+
+function closeNav() {
+    document.getElementById("myBottomnav").style.height = "0";
 }
